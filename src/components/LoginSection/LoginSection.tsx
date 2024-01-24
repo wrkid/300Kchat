4cf039ea658ef95a701e04106b83fdda0fd31173
Alexey Kukrus <107053756+AlexeyKukrus@users.noreply.github.com>
2024-01-24 23:40:41 +0300

    Настройка eslint prettier hasky lint-staged

--- src/components/LoginSection/LoginSection.tsx
+++ src/components/LoginSection/LoginSection.tsx
@@ -1,13 +1,13 @@
 import FormButton from '../../UiKit/FormButton/FormButton';
 import FormInput from '../../UiKit/FormInput';
 import './index.scss'
-const login = require('../../assets/img/login.png')
+import login from '../../assets/img/login.png';
 
 const LoginSection = () => {
   return (
     <div className="login-section">
       <div className='login-section__inner'>
-        <img src={String(login)}></img>
+        <img src={login} alt="login"></img>
         <span>300K Chat</span>
           <FormInput type='email'/>
           <FormInput type='password'/>
