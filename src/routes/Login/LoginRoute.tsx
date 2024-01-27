050ad35e53b0d47cde717cf6b539178cd41bc57b
Alexey Kukrus <107053756+AlexeyKukrus@users.noreply.github.com>
2024-01-27 08:09:48 +0300

    setup routing (/login, /reg)

---
+++ src/routes/Login/LoginRoute.tsx
@@ -0,0 +1,10 @@
+import LoginSection from '../../components/LoginSection';
+import './index.scss';
+
+const LoginRoute = () => {
+  return (
+    <LoginSection />
+  )
+}
+
+export default LoginRoute;
---
+++ src/routes/Registration/RegistrationRoute.tsx
@@ -0,0 +1,10 @@
+import RegisterSection from '../../components/RegisterSection';
+import './index.scss';
+
+const RegistrationRoute = () => {
+  return (
+    <RegisterSection />
+  )
+}
+
+export default RegistrationRoute;
